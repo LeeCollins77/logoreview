bffcd819aa1716d174f3352309946881127bde0f
LeeCollins77 <vixenshare@gmail.com>
2022-01-11 09:59:04 -0500

    Added files from hw20
    Added files from HW20 into folder

---
+++ js/testanimatejs.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+$('.header').animate({'opacity':'1'},1000);
+
+});
+
+
+
+
+
+/* var img = document.querySelector("img");
+document.body.classList.add('js-loading');
+img.addEventListener("load", removeLoadingClass);
+function removeLoadingClass() {
+  document.body.classList.remove('js-loading');
+}
+  
+
+
+const images = document.getElementsByTagName("img");
+for (let image of images) {
+  image.addEventListener("load", fadeImg);
+  image.style.opacity = "0";
+}
+
+function fadeImg () {
+  this.style.transition = "opacity 2s";
+  this.style.opacity = "1";
+}
+
+*/
